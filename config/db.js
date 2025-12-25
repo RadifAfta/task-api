dd7346733392f7317b0287e91774fccdc6203334
RadifAfta <trydi2611@gmail.com>
2025-12-26 00:33:59 +0700

    Setup production : Integrasi dengan supabase postgres

--- config/db.js
+++ config/db.js
@@ -6,11 +6,7 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
     ssl: process.env.PG_SSL === 'true' ? { rejectUnauthorized: false } : false,
 });
 
